0f559489b443dc3691ac75f34681904e9d42c681
Joe Nace <jwnace@gmail.com>
2022-11-30 17:02:57 -0500

    initial solution for day 1

---
+++ src/aoc-2018-csharp/Day01/Day01.cs
@@ -0,0 +1,37 @@
+﻿namespace aoc_2018_csharp.Day01;
+
+public static class Day01
+{
+    private static readonly string[] Input = File.ReadAllLines("Day01/day01.txt");
+
+    public static int Part1()
+    {
+        var temp = Input.Select(int.Parse);
+
+        return temp.Sum(x => x);
+        
+    }
+
+    public static int Part2()
+    {
+        var temp = Input.Select(int.Parse);
+
+        var knownValues = new List<int>();
+        var frequency = 0;
+        
+        while(true)
+        foreach(var t in temp)
+        {
+            frequency = frequency + t;
+
+            if (knownValues.Contains(frequency))
+            {
+                return frequency;
+            }
+
+            knownValues.Add(frequency);
+        }
+
+        return 0;
+    }
+}
---
+++ src/aoc-2018-csharp/Program.cs
@@ -0,0 +1,2 @@
+﻿Console.WriteLine($"Day 01, Part 1: {Day01.Part1()}");
+Console.WriteLine($"Day 01, Part 2: {Day01.Part2()}");
