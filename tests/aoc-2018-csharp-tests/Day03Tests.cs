ec8d8d207fc6db7e19db42a65fa25614784d12b7
Joe Nace <jwnace@gmail.com>
2023-01-05 11:02:01 -0500

    updated tests for day 3

--- tests/aoc-2018-csharp-tests/Day03Tests.cs
+++ tests/aoc-2018-csharp-tests/Day03Tests.cs
@@ -7,7 +7,7 @@ public class Day03Tests
     [Test]
     public void Part1_ReturnsCorrectAnswer()
     {
-        var expected = 0;
+        var expected = 107_663;
         var actual = Day03.Part1();
         actual.Should().Be(expected);
     }
@@ -15,7 +15,7 @@ public class Day03Tests
     [Test]
     public void Part2_ReturnsCorrectAnswer()
     {
-        var expected = 0;
+        var expected = 1_166;
         var actual = Day03.Part2();
         actual.Should().Be(expected);
     }
