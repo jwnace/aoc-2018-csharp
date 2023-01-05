ac0e2e502651243e5484986c7b318966feb0a2f0
Joe Nace <jwnace@gmail.com>
2023-01-05 15:22:02 -0500

    added solution for day 5

--- src/aoc-2018-csharp/Day05/Day05.cs
+++ src/aoc-2018-csharp/Day05/Day05.cs
@@ -2,9 +2,54 @@ namespace aoc_2018_csharp.Day05;
 
 public static class Day05
 {
-    private static readonly string[] Input = File.ReadAllLines("Day05/day05.txt");
+    private static readonly string Input = File.ReadAllText("Day05/day05.txt");
 
-    public static int Part1() => 1;
+    public static int Part1()
+    {
+        var input = Input.ToList();
+        return GetShortenedLength(input);
+    }
 
-    public static int Part2() => 2;
+    public static int Part2()
+    {
+        var best = int.MaxValue;
+        
+        for (var c = 'a'; c <= 'z'; c++)
+        {
+            var input = Input.ToList();
+            input.RemoveAll(x => x == c || x == c - 32);
+            best = Math.Min(best, GetShortenedLength(input));
+        }
+        
+        return best;
+    }
+
+    private static int GetShortenedLength(List<char> input)
+    {
+        while (true)
+        {
+            var reactionHappened = false;
+
+            for (var i = 0; i < input.Count - 1; i++)
+            {
+                var (left, right) = (input[i], input[i + 1]);
+                var difference = Math.Abs(left - right);
+
+                if (difference == 32)
+                {
+                    input.RemoveAt(i + 1);
+                    input.RemoveAt(i);
+                    reactionHappened = true;
+                    break;
+                }
+            }
+
+            if (reactionHappened == false)
+            {
+                break;
+            }
+        }
+
+        return input.Count;
+    }
 }
--- tests/aoc-2018-csharp-tests/Day05Tests.cs
+++ tests/aoc-2018-csharp-tests/Day05Tests.cs
@@ -7,7 +7,7 @@ public class Day05Tests
     [Test]
     public void Part1_ReturnsCorrectAnswer()
     {
-        var expected = 0;
+        var expected = 9_562;
         var actual = Day05.Part1();
         actual.Should().Be(expected);
     }
@@ -15,7 +15,7 @@ public class Day05Tests
     [Test]
     public void Part2_ReturnsCorrectAnswer()
     {
-        var expected = 0;
+        var expected = 4_934;
         var actual = Day05.Part2();
         actual.Should().Be(expected);
     }
