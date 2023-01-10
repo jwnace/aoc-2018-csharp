08370eba0f23797d2b989502cbd9b8bc7a551626
Joe Nace <jwnace@gmail.com>
2023-01-09 23:13:57 -0500

    added solution for day 8

--- src/aoc-2018-csharp/Day08/Day08.cs
+++ src/aoc-2018-csharp/Day08/Day08.cs
@@ -2,9 +2,75 @@
 
 public static class Day08
 {
-    private static readonly string[] Input = File.ReadAllLines("Day08/day08.txt");
+    private static readonly int[] Input = File.ReadAllText("Day08/day08.txt").Split(' ').Select(int.Parse).ToArray();
 
-    public static int Part1() => 1;
+    public static int Part1() => BuildTree().GetMetadataRecursive().Sum();
 
-    public static int Part2() => 2;
+    public static int Part2() => BuildTree().GetValueRecursive();
+
+    private static Node BuildTree()
+    {
+        var index = 0;
+        return BuildTree(ref index);
+    }
+
+    private static Node BuildTree(ref int index)
+    {
+        var node = new Node();
+        var childNodeCount = Input[index];
+        var metadataCount = Input[++index];
+
+        while (node.Children.Count < childNodeCount)
+        {
+            index++;
+            node.Children.Add(BuildTree(ref index));
+        }
+
+        while (node.Metadata.Count < metadataCount)
+        {
+            node.Metadata.Add(Input[++index]);
+        }
+
+        return node;
+    }
+
+    private class Node
+    {
+        public List<Node> Children { get; } = new();
+        public List<int> Metadata { get; } = new();
+
+        public List<int> GetMetadataRecursive()
+        {
+            var result = new List<int>();
+
+            result.AddRange(Metadata);
+
+            foreach (var child in Children)
+            {
+                result.AddRange(child.GetMetadataRecursive());
+            }
+
+            return result;
+        }
+
+        public int GetValueRecursive()
+        {
+            if (Children.Count == 0)
+            {
+                return Metadata.Sum();
+            }
+
+            var result = 0;
+
+            foreach (var metadata in Metadata)
+            {
+                var index = metadata - 1;
+                var child = Children.ElementAtOrDefault(index);
+
+                result += child?.GetValueRecursive() ?? 0;
+            }
+
+            return result;
+        }
+    }
 }
