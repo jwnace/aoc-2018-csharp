0f2e83cf591ad17941171a1b8b6a3bffdc3ca70c
Joe Nace <jwnace@gmail.com>
2023-01-11 10:03:39 -0500

    added solution for part 1

--- src/aoc-2018-csharp/Day09/Day09.cs
+++ src/aoc-2018-csharp/Day09/Day09.cs
@@ -4,7 +4,73 @@ public static class Day09
 {
     private static readonly string[] Input = File.ReadAllLines("Day09/day09.txt");
 
-    public static int Part1() => 1;
+    public static int Part1() => GetMaxScore(403, 71_920);
 
     public static int Part2() => 2;
+    
+    public static int GetMaxScore(int playerCount, int lastMarbleValue)
+    {
+        var scores = new int[playerCount];
+        Array.Fill(scores, 0);
+
+        var nodes = new List<Node>();
+
+        var root = new Node(0);
+        root.Next = root;
+        root.Previous = root;
+
+        nodes.Add(root);
+
+        var currentNode = root;
+
+        for (var i = 1; i <= lastMarbleValue; i++)
+        {
+            var player = (i - 1) % playerCount;
+
+            if (i % 23 == 0)
+            {
+                var nodeToRemove = currentNode.Previous.Previous.Previous.Previous.Previous.Previous.Previous;
+
+                var left = nodeToRemove.Previous;
+                var right = nodeToRemove.Next;
+
+                left.Next = right;
+                right.Previous = left;
+
+                currentNode = right;
+
+                scores[player] += i + nodeToRemove.Value;
+            }
+            else
+            {
+                var left = currentNode.Next;
+                var right = currentNode.Next.Next;
+                var node = new Node(i);
+
+                left.Next = node;
+                node.Next = right;
+
+                right.Previous = node;
+                node.Previous = left;
+
+                nodes.Add(node);
+
+                currentNode = node;
+            }
+        }
+
+        return scores.Max();
+    }
+    
+    private class Node
+    {
+        public int Value { get; }
+        public Node? Next { get; set; }
+        public Node? Previous { get; set; }
+
+        public Node(int value)
+        {
+            Value = value;
+        }
+    }
 }
--- tests/aoc-2018-csharp-tests/Day09Tests.cs
+++ tests/aoc-2018-csharp-tests/Day09Tests.cs
@@ -4,10 +4,22 @@ namespace aoc_2018_csharp_tests;
 
 public class Day09Tests
 {
+    [TestCase(9, 25, 32)]
+    [TestCase(10, 1_618, 8_317)]
+    [TestCase(13, 7_999, 146_373)]
+    [TestCase(17, 1_104, 2_764)]
+    [TestCase(21, 6_111, 54_718)]
+    [TestCase(30, 5_807, 37_305)]
+    public void GetMaxScore_ReturnsCorrectAnswer(int playerCount, int lastMarbleValue, int expected)
+    {
+        var actual = Day09.GetMaxScore(playerCount, lastMarbleValue);
+        actual.Should().Be(expected);
+    }
+    
     [Test]
     public void Part1_ReturnsCorrectAnswer()
     {
-        var expected = 0;
+        var expected = 439_089;
         var actual = Day09.Part1();
         actual.Should().Be(expected);
     }
