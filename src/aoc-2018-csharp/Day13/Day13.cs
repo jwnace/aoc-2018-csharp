7a42f5855136c7a94d839fc05d8762e00aff8dce
Joe Nace <jwnace@gmail.com>
2023-04-05 00:36:39 -0400

    added placeholder for day 13 solution

---
+++ src/aoc-2018-csharp/Day13/Day13.cs
@@ -0,0 +1,8 @@
+﻿namespace aoc_2018_csharp.Day13;
+
+public static class Day13
+{
+    public static int Part1() => 1;
+
+    public static int Part2() => 2;
+}
--- src/aoc-2018-csharp/GlobalUsings.cs
+++ src/aoc-2018-csharp/GlobalUsings.cs
@@ -10,7 +10,7 @@ global using aoc_2018_csharp.Day09;
 global using aoc_2018_csharp.Day10;
 global using aoc_2018_csharp.Day11;
 global using aoc_2018_csharp.Day12;
-// global using aoc_2018_csharp.Day13;
+global using aoc_2018_csharp.Day13;
 // global using aoc_2018_csharp.Day14;
 // global using aoc_2018_csharp.Day15;
 // global using aoc_2018_csharp.Day16;
