da697935eac03c314d6fc3bfc413f67f127056c8
Joe Nace <jwnace@gmail.com>
2023-04-06 00:01:48 -0400

    added placeholder for day 14 solution

---
+++ src/aoc-2018-csharp/Day14/Day14.cs
@@ -0,0 +1,10 @@
+﻿namespace aoc_2018_csharp.Day14;
+
+public class Day14
+{
+    private static readonly string[] Input = File.ReadAllLines("Day14/day14.txt");
+
+    public static int Part1() => 1;
+
+    public static int Part2() => 2;
+}
--- src/aoc-2018-csharp/GlobalUsings.cs
+++ src/aoc-2018-csharp/GlobalUsings.cs
@@ -11,7 +11,7 @@ global using aoc_2018_csharp.Day10;
 global using aoc_2018_csharp.Day11;
 global using aoc_2018_csharp.Day12;
 global using aoc_2018_csharp.Day13;
-// global using aoc_2018_csharp.Day14;
+global using aoc_2018_csharp.Day14;
 // global using aoc_2018_csharp.Day15;
 // global using aoc_2018_csharp.Day16;
 // global using aoc_2018_csharp.Day17;
