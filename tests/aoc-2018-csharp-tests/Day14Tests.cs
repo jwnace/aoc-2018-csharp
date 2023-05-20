5ea45dc588335ff1d9c6e7f3a2adfcd8536399ff
Joe Nace <jwnace@gmail.com>
2023-05-19 23:39:26 -0400

    added solution for day 14

--- tests/aoc-2018-csharp-tests/Day14Tests.cs
+++ tests/aoc-2018-csharp-tests/Day14Tests.cs
@@ -7,16 +7,26 @@ public class Day14Tests
     [Test]
     public void Part1_ReturnsCorrectAnswer()
     {
-        var expected = 0;
+        // arrange
+        var expected = 9_315_164_154;
+
+        // act
         var actual = Day14.Part1();
+
+        // assert
         actual.Should().Be(expected);
     }
 
     [Test]
     public void Part2_ReturnsCorrectAnswer()
     {
-        var expected = 0;
+        // arrange
+        var expected = 20_231_866;
+
+        // act
         var actual = Day14.Part2();
+
+        // assert
         actual.Should().Be(expected);
     }
 }
