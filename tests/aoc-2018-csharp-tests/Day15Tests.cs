f83e42971962d28f4255cd6864d02b33088f4a2e
Joe Nace <jwnace@gmail.com>
2023-05-19 23:46:56 -0400

    added placeholder for day 15 solution

---
+++ tests/aoc-2018-csharp-tests/Day15Tests.cs
@@ -0,0 +1,32 @@
+﻿using aoc_2018_csharp.Day15;
+
+namespace aoc_2018_csharp_tests;
+
+public class Day15Tests
+{
+    [Test]
+    public void Part1_ReturnsCorrectAnswer()
+    {
+        // arrange
+        var expected = 0;
+
+        // act
+        var actual = Day15.Part1();
+
+        // assert
+        actual.Should().Be(expected);
+    }
+
+    [Test]
+    public void Part2_ReturnsCorrectAnswer()
+    {
+        // arrange
+        var expected = 0;
+
+        // act
+        var actual = Day15.Part2();
+
+        // assert
+        actual.Should().Be(expected);
+    }
+}
