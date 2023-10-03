fa6253017e102f5339c6bbd48a2813874a6505a2
Joe Nace <jwnace@gmail.com>
2023-10-03 17:48:56 -0400

    added placeholder for day 16 solution

---
+++ src/aoc-2018-csharp/Day16/Day16.cs
@@ -0,0 +1,16 @@
+namespace aoc_2018_csharp.Day16;
+
+public static class Day16
+{
+    public static readonly string[] Input = File.ReadAllLines("Day16/day16.txt");
+
+    public static int Part1()
+    {
+        throw new NotImplementedException();
+    }
+
+    public static int Part2()
+    {
+        throw new NotImplementedException();
+    }
+}
