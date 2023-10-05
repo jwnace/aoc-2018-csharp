45090e63b1afa9eff47cfee313c68466d8f82522
Joe Nace <jwnace@gmail.com>
2023-10-04 20:24:04 -0400

    added test for part 1 example

--- tests/aoc-2018-csharp-tests/Day17Tests.cs
+++ tests/aoc-2018-csharp-tests/Day17Tests.cs
@@ -5,6 +5,24 @@ namespace aoc_2018_csharp_tests;
 public class Day17Tests
 {
     [Test]
+    public void Part1_Example_ReturnsCorrectAnswer()
+    {
+        var input = new[]
+        {
+            "x=495, y=2..7",
+            "y=7, x=495..501",
+            "x=501, y=3..7",
+            "x=498, y=2..4",
+            "x=506, y=1..2",
+            "x=498, y=10..13",
+            "x=504, y=10..13",
+            "y=13, x=498..504",
+        };
+
+        Day17.Solve1(input).Should().Be(57);
+    }
+
+    [Test]
     public void Part1_ReturnsCorrectAnswer()
     {
         Day17.Part1().Should().Be(0);
