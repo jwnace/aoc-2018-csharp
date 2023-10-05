96d77c038d2bd1465bbe7134cde0576a99ff7686
Joe Nace <jwnace@gmail.com>
2023-10-05 14:59:33 -0400

    updated tests for part 2

--- tests/aoc-2018-csharp-tests/Day17Tests.cs
+++ tests/aoc-2018-csharp-tests/Day17Tests.cs
@@ -42,12 +42,30 @@ public class Day17Tests
     [Test]
     public void Part1_ReturnsCorrectAnswer()
     {
-        Day17.Part1().Should().Be(0);
+        Day17.Part1().Should().Be(27331);
+    }
+
+    [Test]
+    public void Part2_Example_ReturnsCorrectAnswer()
+    {
+        var input = new[]
+        {
+            "x=495, y=2..7",
+            "y=7, x=495..501",
+            "x=501, y=3..7",
+            "x=498, y=2..4",
+            "x=506, y=1..2",
+            "x=498, y=10..13",
+            "x=504, y=10..13",
+            "y=13, x=498..504",
+        };
+
+        Day17.Solve2(input).Should().Be(29);
     }
 
     [Test]
     public void Part2_ReturnsCorrectAnswer()
     {
-        Day17.Part2().Should().Be(0);
+        Day17.Part2().Should().Be(22245);
     }
 }
