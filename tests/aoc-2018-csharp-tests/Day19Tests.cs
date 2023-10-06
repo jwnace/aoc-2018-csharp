7fad0e86159f268ccae907c6694b5f4817b34405
Joe Nace <jwnace@gmail.com>
2023-10-06 17:59:28 -0400

    updated test for part 1 example

--- tests/aoc-2018-csharp-tests/Day19Tests.cs
+++ tests/aoc-2018-csharp-tests/Day19Tests.cs
@@ -9,10 +9,17 @@ public class Day19Tests
     {
         var input = new[]
         {
-            "",
+            "#ip 0",
+            "seti 5 0 1",
+            "seti 6 0 2",
+            "addi 0 1 0",
+            "addr 1 2 3",
+            "setr 1 0 0",
+            "seti 8 0 4",
+            "seti 9 0 5",
         };
 
-        Day19.Solve1(input).Should().Be(0);
+        Day19.Solve1(input).Should().Be(6);
     }
 
     [Test]
