c6d1e87735c08b1f9b7d5e434f980dbcf02d84be
Joe Nace <jwnace@gmail.com>
2023-10-08 01:05:29 -0400

    WIP - started implementing solution for day 20 part 1

--- src/aoc-2018-csharp/Day20/Day20.cs
+++ src/aoc-2018-csharp/Day20/Day20.cs
@@ -1,20 +1,175 @@
-﻿namespace aoc_2018_csharp.Day20;
+﻿using System.Text;
+
+namespace aoc_2018_csharp.Day20;
 
 public static class Day20
 {
-    private static readonly string[] Input = File.ReadAllLines("Day20/day20.txt");
+    private static readonly string Input = File.ReadAllText("Day20/day20.txt").Trim();
 
     public static int Part1() => Solve1(Input);
 
     public static int Part2() => Solve2(Input);
 
-    public static int Solve1(string[] input)
+    public static int Solve1(string input)
+    {
+        var rooms = new List<Room>();
+        var (row, col) = (0, 0);
+        var room = new Room(row, col);
+        rooms.Add(room);
+
+        for (var i = 1; i < input.Length - 1; i++)
+        {
+            switch (input[i])
+            {
+                case 'N':
+                {
+                    row--;
+                    var newRoom = new Room(row, col);
+                    room.North = newRoom;
+                    newRoom.South = room;
+                    rooms.Add(newRoom);
+                    room = newRoom;
+                    break;
+                }
+                case 'S':
+                {
+                    row++;
+                    var newRoom = new Room(row, col);
+                    room.South = newRoom;
+                    newRoom.North = room;
+                    rooms.Add(newRoom);
+                    room = newRoom;
+                    break;
+                }
+                case 'E':
+                {
+                    col++;
+                    var newRoom = new Room(row, col);
+                    room.East = newRoom;
+                    newRoom.West = room;
+                    rooms.Add(newRoom);
+                    room = newRoom;
+                    break;
+                }
+                case 'W':
+                {
+                    col--;
+                    var newRoom = new Room(row, col);
+                    room.West = newRoom;
+                    newRoom.East = room;
+                    rooms.Add(newRoom);
+                    room = newRoom;
+                    break;
+                }
+                case '(':
+                    throw new NotImplementedException();
+                    break;
+                case ')':
+                    throw new NotImplementedException();
+                    break;
+                case '|':
+                    throw new NotImplementedException();
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+
+        Console.WriteLine(DrawMap(rooms));
+
+        return 0;
+    }
+
+    public static int Solve2(string input)
     {
         throw new NotImplementedException();
     }
 
-    public static int Solve2(string[] input)
+    private static string DrawMap(List<Room> rooms)
     {
-        throw new NotImplementedException();
+        var minCol = rooms.Min(r => r.Col);
+        var maxCol = rooms.Max(r => r.Col);
+        var minRow = rooms.Min(r => r.Row);
+        var maxRow = rooms.Max(r => r.Row);
+
+        var builder = new StringBuilder();
+
+        for (var row = minRow; row <= maxRow; row++)
+        {
+            for (var i = 0; i < 3; i++)
+            {
+                for (var col = minCol; col <= maxCol; col++)
+                {
+                    var room = rooms.FirstOrDefault(room => room.Row == row && room.Col == col);
+
+                    // for (var r1 = 0; r1 < 3; r1++)
+                    // {
+                    for (var c1 = 0; c1 < 3; c1++)
+                    {
+                        if (room == null)
+                        {
+                            builder.Append(' ');
+                        }
+                        else
+                        {
+                            var roomString = room.ToString();
+                            var foo = roomString.Split(Environment.NewLine);
+                            var bar = foo[i];
+                            var baz = bar[c1];
+                            builder.Append(baz);
+                        }
+                    }
+                    // }
+
+                    // builder.AppendLine();
+                }
+
+                builder.AppendLine();
+            }
+
+            // builder.AppendLine();
+        }
+
+        return builder.ToString();
+    }
+
+    private class Room
+    {
+        public int Row { get; }
+        public int Col { get; }
+
+        public Room? North { get; set; }
+        public Room? South { get; set; }
+        public Room? East { get; set; }
+        public Room? West { get; set; }
+
+        public Room(int row, int col)
+        {
+            Row = row;
+            Col = col;
+        }
+
+        public override string ToString()
+        {
+            var builder = new StringBuilder();
+
+            builder.Append('#');
+            builder.Append(North != null ? '^' : '#');
+            builder.Append('#');
+
+            builder.AppendLine();
+
+            builder.Append(West != null ? '<' : '#');
+            builder.Append('.');
+            builder.Append(East != null ? '>' : '#');
+
+            builder.AppendLine();
+
+            builder.Append('#');
+            builder.Append(South != null ? 'v' : '#');
+            builder.Append('#');
+
+            return builder.ToString();
+        }
     }
 }
--- tests/aoc-2018-csharp-tests/Day20Tests.cs
+++ tests/aoc-2018-csharp-tests/Day20Tests.cs
@@ -4,10 +4,26 @@ namespace aoc_2018_csharp_tests;
 
 public class Day20Tests
 {
+    [TestCase("^WNE$", 3)]
+    [TestCase("^ENWWW(NEEE|SSE(EE|N))$", 10)]
+    [TestCase("^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$", 18)]
+    [TestCase("^ESSWWN(E|NNENN(EESS(WNSE|)SSS|WWWSSSSE(SW|NNNE)))$", 23)]
+    [TestCase("^WSSEESWWWNW(S|NENNEEEENN(ESSSSW(NWSW|SSEN)|WSWWN(E|WWS(E|SS))))$", 31)]
+    public void Part1_Example_ReturnsCorrectAnswer(string input, int expected)
+    {
+        Day20.Solve1(input).Should().Be(expected);
+    }
+
     [Test]
     public void Part1_ReturnsCorrectAnswer()
     {
         Day20.Part1().Should().Be(0);
+    }
+
+    [TestCase("")]
+    public void Part2_Example_ReturnsCorrectAnswer(string input)
+    {
+        Day20.Solve1(input).Should().Be(0);
     }
 
     [Test]
