64bdbabbdf92649f31a6fca2fbe69c3128936e24
Joe Nace <jwnace@gmail.com>
2023-10-09 22:48:29 -0400

    updated day 21 solutions to take in the input as a string array and removed example tests since there are no examples

--- src/aoc-2018-csharp/Day21/Day21.cs
+++ src/aoc-2018-csharp/Day21/Day21.cs
@@ -2,13 +2,13 @@ namespace aoc_2018_csharp.Day21;
 
 public static class Day21
 {
-    private static readonly string Input = File.ReadAllText("Day21/day21.txt").Trim();
+    private static readonly string[] Input = File.ReadAllLines("Day21/day21.txt");
 
-    public static int Part1() => Solve1();
+    public static int Part1() => Solve1(Input);
 
-    public static int Part2() => Solve2();
+    public static int Part2() => Solve2(Input);
 
-    public static int Solve1() => throw new NotImplementedException();
+    public static int Solve1(string[] strings) => throw new NotImplementedException();
 
-    public static int Solve2() => throw new NotImplementedException();
+    public static int Solve2(string[] strings) => throw new NotImplementedException();
 }
--- tests/aoc-2018-csharp-tests/Day21Tests.cs
+++ tests/aoc-2018-csharp-tests/Day21Tests.cs
@@ -5,21 +5,9 @@ namespace aoc_2018_csharp_tests;
 public class Day21Tests
 {
     [Test]
-    public void Part1_Example_ReturnsCorrectAnswer()
-    {
-        Day21.Solve1().Should().Be(0);
-    }
-
-    [Test]
     public void Part1_ReturnsCorrectAnswer()
     {
         Day21.Part1().Should().Be(0);
-    }
-
-    [Test]
-    public void Part2_Example_ReturnsCorrectAnswer()
-    {
-        Day21.Solve2().Should().Be(0);
     }
 
     [Test]
