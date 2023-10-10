6d181d5544fab1778c76c5446371e88eebdeb926
Joe Nace <jwnace@gmail.com>
2023-10-10 15:01:49 -0400

    added placeholder for day 22 solution

---
+++ src/aoc-2018-csharp/Day22/Day22.cs
@@ -0,0 +1,8 @@
+namespace aoc_2018_csharp.Day22;
+
+public static class Day22
+{
+    public static int Part1() => throw new NotImplementedException();
+
+    public static int Part2() => throw new NotImplementedException();
+}
---
+++ tests/aoc-2018-csharp-tests/Day22Tests.cs
@@ -0,0 +1,18 @@
+using aoc_2018_csharp.Day22;
+
+namespace aoc_2018_csharp_tests;
+
+public class Day22Tests
+{
+    [Test]
+    public void Part1_ReturnsCorrectAnswer()
+    {
+        Day22.Part1().Should().Be(0);
+    }
+
+    [Test]
+    public void Part2_ReturnsCorrectAnswer()
+    {
+        Day22.Part2().Should().Be(0);
+    }
+}
