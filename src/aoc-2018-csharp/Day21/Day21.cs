84d942d6764998f26f32957b933f901475d70620
Joe Nace <jwnace@gmail.com>
2023-10-09 22:44:55 -0400

    added placeholder for day 21 solution

---
+++ src/aoc-2018-csharp/Day21/Day21.cs
@@ -0,0 +1,14 @@
+namespace aoc_2018_csharp.Day21;
+
+public static class Day21
+{
+    private static readonly string Input = File.ReadAllText("Day21/day21.txt").Trim();
+
+    public static int Part1() => Solve1();
+
+    public static int Part2() => Solve2();
+
+    public static int Solve1() => throw new NotImplementedException();
+
+    public static int Solve2() => throw new NotImplementedException();
+}
---
+++ tests/aoc-2018-csharp-tests/Day21Tests.cs
@@ -0,0 +1,30 @@
+using aoc_2018_csharp.Day21;
+
+namespace aoc_2018_csharp_tests;
+
+public class Day21Tests
+{
+    [Test]
+    public void Part1_Example_ReturnsCorrectAnswer()
+    {
+        Day21.Solve1().Should().Be(0);
+    }
+
+    [Test]
+    public void Part1_ReturnsCorrectAnswer()
+    {
+        Day21.Part1().Should().Be(0);
+    }
+
+    [Test]
+    public void Part2_Example_ReturnsCorrectAnswer()
+    {
+        Day21.Solve2().Should().Be(0);
+    }
+
+    [Test]
+    public void Part2_ReturnsCorrectAnswer()
+    {
+        Day21.Part2().Should().Be(0);
+    }
+}
